a6519fcaedba21efb3ed568c7457833b837f6989
João Lopes <joaovitorlopesilva201@gmail.com>
2026-03-18 18:18:30 -0300

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
+  base: '/COUNTER-not-strike_FLUX_REDUX/',
+
 })
